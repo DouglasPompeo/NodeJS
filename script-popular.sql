a79e3c0e6f477a40cc8e44186a9fffad27f98446
DouglasPompeo01~ <dpompeo20@gmail.com>
2023-08-03 14:32:38 -0300

    atualizado

---
+++ script-popular.sql
@@ -0,0 +1,4 @@
+INSERT INTO portfolio (descriçao,detalhes)
+VALUES ('Desenvolvimento de WebSites' , 'tecnologias JavaScript,NodeJS,Express e MySQL');
+INSERT INTO portfolio(descriçao , detalhes)
+VALUES ('Desenvolvimento de APIs' , 'tecnologias JavaScript,NodeJS,Express e MySQL');
---
+++ script_tab.sql
@@ -0,0 +1,5 @@
+CREATE TABLE portfolio (
+    id_portfolio INTEGER UNSIGNED NOT NULL AUTO_INCREMENT ,
+    descriçao VARCHAR(255) NULL ,
+    detalhes TEXT NULL ,
+    PRIMARY KEY (id_portfolio));
